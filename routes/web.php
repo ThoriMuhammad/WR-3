4a76a998f7da488e3b2675d23ce73d5d2f56e6a9
ThoriMuhammad <thorimuhammad08@gmail.com>
2024-08-20 16:56:57 +0700

    Terbaru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,3 +9,8 @@ Route::get('/', function () {
 Route::get('/menu', function () {
     return view('Pegawai.menu');
 });
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
