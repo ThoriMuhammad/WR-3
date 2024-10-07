d0a9420bf1df35bbb1e45a8c55e1960e7648ad32
Rijalai091 <Rijalaimanhanif@students.amikom.ac.id>
2024-10-07 15:53:55 +0700

    new

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,7 @@
     <div class="container mt-4">
       @yield('content')
     </div>
+    @include('layouts.footer')
     @yield('scripts')
     <script src="{{ asset('assets/js/script.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="{{ asset('assets/css/footer.css') }}" rel="stylesheet">
+    <!-- Ikon Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
+  </head>
+  <body>
+  <footer>
+    <div class="footer-content">
+      <h3>INSTITUT TEKNOLOGI YOGYAKARTA</h3>
+      <div  style="margin-top: 50px;">
+      <h3>KAMPUS 1</h3>
+      <p>
+Fakultas Teknik Lingkungan dan Sumber Daya Alam
+Jl. Janti Km 4 Gedongkuning, Banguntapan, DIY
+Telp. (0274) 566863, email: info@ity.ac.id</p>
+      
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2024 Eternity Coffee | Desain oleh Nama Anda</p>
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
